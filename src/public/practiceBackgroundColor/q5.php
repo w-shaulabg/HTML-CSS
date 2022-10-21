830bd7857396f0cca6122c860dc87bb05f68f2a2
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-10-21 19:51:52 +0900

    5. CSSを触ってみよう背景色の変更課題２から５

--- src/public/practiceBackgroundColor/q5.php
+++ src/public/practiceBackgroundColor/q5.php
@@ -6,11 +6,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="../css/practiceBackgroundColor/example_q5.css">
 </head>
 
 <body>
   <main>
-    
+    <div class="main_1">
+      <span class="main__text--white">内容１</span>
+      <span class="main__text--orange">内容２</span>
+    </div>
+    <div class="main_2">
+      <span class="main__text--yellow">内容３</span>
+      <span class="main__text--white">内容４</span>
+    </div>
   </main>
 </body>
 
