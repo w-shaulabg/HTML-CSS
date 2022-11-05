4bf16e5ba201c2723db03b4278866bdd89bb54ed
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-11-05 17:23:26 +0900

    ⑦display: flex; で横並び(中級)課題１から５

--- src/public/practiceFlexIntermediate/q1.php
+++ src/public/practiceFlexIntermediate/q1.php
@@ -5,11 +5,62 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/practiceFlexIntermediate/q1.css">
   <title>Document</title>
 </head>
 
 <body>
-  
+  <main>
+
+    <div class="topButtonsWrapper">
+
+      <div>
+        <button class="button">ボタン1</button>
+      </div>
+
+      <div>
+        <button class="button">ボタン1</button>
+      </div>
+
+      <div>
+        <button class="button">ボタン1</button>
+      </div>
+
+    </div>
+
+    <div class="middleButtonsWrapper">
+
+      <div>
+        <button class="button">ボタン2</button>
+      </div>
+
+      <div>
+        <button class="button">ボタン2</button>
+      </div>
+
+      <div>
+        <button class="button">ボタン2</button>
+      </div>
+
+    </div>
+
+    <div class="bottomButtonsWrapper">
+
+      <div>
+        <button class="button">ボタン3</button>
+      </div>
+
+      <div>
+        <button class="button">ボタン3</button>
+      </div>
+
+      <div>
+        <button class="button">ボタン3</button>
+      </div>
+
+    </div>
+
+  </main>
 </body>
 
 </html>
---
+++ src/public/practiceFlexIntermediate/q5.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/practiceFlexIntermediate/q5.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <main class="main">
+
+    <div class="topButtonsWrapper">
+      <div class="Button1">
+        <button class="button">レフトボタン</button>
+      </div>
+    </div>
+
+    <div>
+      <div class="button1">
+        <button class="button">ライトボタン1</button>
+      </div>
+      <div class=rightBlock>
+        <div>
+          <button class="button">ライトボタン2</button>
+        </div>
+        <div>
+          <button class="button">ライトボタン3</button>
+        </div>
+      </div>
+    </div>
+
+  </main>
+</body>
+
+</html>
