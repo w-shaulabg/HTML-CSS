f3cde104529cbe2604b8d0083bcb07f0c131eebb
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-11-29 20:16:55 +0900

    特別課題模写コーディング(方向性確認)

---
+++ src/public/reproductionHtml/reproduction.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../reproductionCSS/reproduction.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <div class="flex">
+      <div>
+        <a href="reproduction.php"> <img class="profile" src="https://code-step.com/demo/html/profile/img/logo.svg"
+            alt="profile"></a>
+      </div>
+      <nav>
+        <ul class="flex2">
+          <li class="margin1">
+            About
+          </li>
+          <li class="marginBicycle">
+            Bicycle
+          </li>
+        </ul>
+      </nav>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <div class="marginBicycleUnder">
+      <img class="bicycle" src="https://code-step.com/demo/html/profile/img/mainvisual.jpg" alt="テキストテキストテキスト">
+    </div>
+
+    <h1 class="marginBicycleUnderAbout">About</h1>
+    <div class="flex3">
+      <div>
+        <img class="guy" src="https://code-step.com/demo/html/profile/img/about.jpg" alt="テキストテキストテキスト">
+      </div>
+      <div class="nameAndText">
+        <h1 class="fullName">SAITO TAKAHIRO</h1>
+        <div>テキストテキストテキストテキストテキストテキストテキスト</div>
+        <div>テキストテキストテキストテキストテキストテキストテキスト</div>
+        <div>テキストテキストテキストテキストテキストテキストテキスト</div>
+      </div>
+    </div>
+    <h2 class="marginBicycleUnderAbout">Bicycle</h2>
+
+    <li class="flex2">
+      <div>
+        <img src="https://code-step.com/demo/html/profile/img/bicycle1.jpg" alt="テキストテキストテキスト">
+        <h3>タイトルタイトル</h3>
+        <p>テキストテキストテキスト</p>
+      </div>
+      <div>
+        <img src="https://code-step.com/demo/html/profile/img/bicycle2.jpg" alt="テキストテキスト">
+        <h3>タイトルタイトル</h3>
+        <p>テキストテキストテキスト</p>
+      </div>
+    </li>
+
+
+  </main>
+</body>
