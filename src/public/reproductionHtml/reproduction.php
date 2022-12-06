657010edcf2f427214c9d40abfbef4ac6600aed6
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-12-06 22:07:18 +0900

    12/6模写コーディング進捗状況

--- src/public/reproductionHtml/reproduction.php
+++ src/public/reproductionHtml/reproduction.php
@@ -6,62 +6,90 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../reproductionCSS/reproduction.css">
-  <title>Document</title>
+  <link rel="shortcut icon" href="../reproductionImg/favicon.ico">
+  <title>Profile</title>
 </head>
 
+
 <body>
-  <header>
-    <div class="flex">
-      <div>
-        <a href="reproduction.php"> <img class="profile" src="https://code-step.com/demo/html/profile/img/logo.svg"
-            alt="profile"></a>
-      </div>
-      <nav>
-        <ul class="flex2">
-          <li class="margin1">
-            About
-          </li>
-          <li class="marginBicycle">
-            Bicycle
-          </li>
-        </ul>
-      </nav>
-      </nav>
+
+  <header class="flex">
+    <div>
+      <a href="reproduction.php"> <img class="profile" src="https://code-step.com/demo/html/profile/img/logo.svg"
+          alt="profile"></a>
     </div>
+
+    <ul class="flex2">
+      <li>
+        <a class="underline" href="#about">About</a>
+      </li>
+
+      <li class="margin-left30px">
+        <a class="underline" href="#bicycle">Bicycle</a>
+      </li>
+    </ul>
+
   </header>
 
   <main>
-    <div class="marginBicycleUnder">
+
+    <div class="margin-bottom100">
       <img class="bicycle" src="https://code-step.com/demo/html/profile/img/mainvisual.jpg" alt="テキストテキストテキスト">
     </div>
 
-    <h1 class="marginBicycleUnderAbout">About</h1>
-    <div class="flex3">
-      <div>
-        <img class="guy" src="https://code-step.com/demo/html/profile/img/about.jpg" alt="テキストテキストテキスト">
-      </div>
-      <div class="nameAndText">
-        <h1 class="fullName">SAITO TAKAHIRO</h1>
-        <div>テキストテキストテキストテキストテキストテキストテキスト</div>
-        <div>テキストテキストテキストテキストテキストテキストテキスト</div>
-        <div>テキストテキストテキストテキストテキストテキストテキスト</div>
-      </div>
-    </div>
-    <h2 class="marginBicycleUnderAbout">Bicycle</h2>
+    <section class="center">
 
-    <li class="flex2">
-      <div>
-        <img src="https://code-step.com/demo/html/profile/img/bicycle1.jpg" alt="テキストテキストテキスト">
-        <h3>タイトルタイトル</h3>
-        <p>テキストテキストテキスト</p>
-      </div>
-      <div>
-        <img src="https://code-step.com/demo/html/profile/img/bicycle2.jpg" alt="テキストテキスト">
-        <h3>タイトルタイトル</h3>
-        <p>テキストテキストテキスト</p>
-      </div>
-    </li>
+      <h1 id="about" class="text-underline">About</h1>
 
+      <div class="flex3">
+
+        <div>
+          <img class="guy" src="https://code-step.com/demo/html/profile/img/about.jpg" alt="テキストテキストテキスト">
+        </div>
+
+        <div class="nameAndText">
+          <h1 class="Margin-10-0">SAITO TAKAHIRO</h1>
+          <div>テキストテキストテキストテキストテキストテキストテキスト</div>
+          <div>テキストテキストテキストテキストテキストテキストテキスト</div>
+          <div>テキストテキストテキストテキストテキストテキストテキスト</div>
+        </div>
+
+      </div>
+
+    </section>
+
+    <section class="center">
+
+      <h2 id="bicycle" class="text-underline margin-bottom">Bicycle</h2>
+
+      <ul class="flex4 li">
+
+        <li class="bicycle-li">
+          <img src="https://code-step.com/demo/html/profile/img/bicycle1.jpg" alt="テキストテキスト">
+          <h3 class="title">タイトルタイトル</h3>
+          <p>テキストテキストテキスト</p>
+        </li>
+
+        <li class="bicycle-li">
+          <img src="https://code-step.com/demo/html/profile/img/bicycle2.jpg" alt="テキストテキスト">
+          <h3 class="title">タイトルタイトル</h3>
+          <p>テキストテキストテキスト</p>
+        </li>
+
+        <li class="bicycle-li">
+          <img src="https://code-step.com/demo/html/profile/img/bicycle3.jpg" alt="テキストテキスト">
+          <h3 class="title">タイトルタイトル</h3>
+          <p>テキストテキストテキスト</p>
+        </li>
+
+      </ul>
+
+    </section>
 
   </main>
+
+  <footer class="footer">
+    <p>© 2022 Profile</p>
+  </footer>
+
 </body>
