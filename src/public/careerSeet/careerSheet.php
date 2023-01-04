df55abe4b2459dc28e132762818d1da3c573a4eb
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2023-01-04 11:04:35 +0900

    ポートフォリオ

---
+++ src/public/careerSeet/careerSheet.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./careerSheet.css">
+  <title>職務経歴書</title>
+</head>
+
+<body>
+  <section class="page">
+    <header>
+      <h1>職務経歴書</h1>
+      <div class="header__name">
+        <div>
+          2023年1月1日現在
+        </div>
+        <div>
+          <span class="name">氏名</span><span>齊藤</span><span>嵩大</span>
+        </div>
+      </div>
+      <div class="header__textContent">
+        <div class="header__textContent__title">
+          <h2>■職務概要</h2>
+        </div>
+        <div class="font__size">
+          <p>入隊後、約4ヵ月間横須賀教育にて教育及び訓練を修業し、八戸航空基地に配属後は航空機の整備に従事。</p>
+          <p>海上自衛隊任期満了後、國際航空給油に入社し最初の配属先は釧路空港で、航空燃料の施設管理や品質検査及び航空機 への燃料搭載作業習得。</p>
+          <p>2年後に新千歳空港へ移動し、 配置換え訓練を経て新千歳空港へ就航する様々な航空会社の機体へ燃料搭載作業従事。 </p>
+          <p>5年間勤務した後、測機社に入社。ダムの観測機器の点検等や地すべりの観測機器の点検等に従事。</P>
+          <p>翌年、黒松内名水で ミネラルウォーターの製造に従事していたが、プログラミングの勉強に専念したいと思い退職を決意。現在は派遣会社に登録し生活費を稼ぎつつプログラミングスクールで技術を習得中。</p>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <div class="main__text__content">
+        <ul class="main__list__title">
+          <li>
+            <h2><span class="main__job__title">2010年4月~2015年3月</span><span>海上自衛隊</span></h2>
+          </li>
+          <li class="main__wrapper">
+            <span>主な業務内容：</span><span>八戸航空基地にて対戦哨戒機の武装機器の装備及び整備</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="main">
+        <div class="main__period">
+          <ul class="list__style1">
+            <li>■期間</li>
+            <li>2010年4月1日</li>
+            <li>2010年8月10日 </li>
+            <li>2012年1月16日~2012年4月10日</li>
+            <li>2012年7月1日~2012年9月30日</li>
+            <li>2012年11月1日 </li>
+            <li>2012年7月1日~2012年9月30日</li>
+            <li>2015年3月31日</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul class="list__style2">
+            <li>■内容</li>
+            <li>横須賀教育隊に入隊し、自衛官として基礎及び基本動作を学ぶ。</li>
+            <li>第2整備補給隊勤務を命ぜられ、第2武器整備隊武器班に配置される。 航空武器に関する整備作業に従事。</li>
+            <li>海上自衛隊第3術科学校に入隊を命ぜられ、航空武器整備の基礎を学ぶと共に体力練成を行う。</li>
+            <li>業務支援のため、第2航空群司令部臨時勤務。 作戦計画幕僚の補佐。</li>
+            <li>第2武器整備隊救命機材班に配置される。航空機へ搭載する救命器材の整備を実施。</li>
+            <li>業務支援のため、第2航空群司令部臨時勤務。 作戦計画幕僚の補佐。</li>
+            <li>任期満了退職。</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="main__text__content">
+        <ul class="main__list__title">
+          <li>
+            <h2><span class="main__job__title">2015年4月~2020年4月</span><span>國際航空給油株式会社</span></h2>
+          </li>
+          <li class="main__wrapper">
+            <span class="main__job__title">主な業務内容：</span><span>航空機への燃料搭載作業及び燃料航空燃料の管理並びに品質検査</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="main">
+        <div class="main__period">
+          <ul class="list__style1">
+            <li>■期間</li>
+            <li>2015年4月1日</li>
+            <li>2015年6月</li>
+            <li>2016年1月</li>
+            <li>&emsp;</li>
+            <li>2016年5月</li>
+            <li>2017年8月</li>
+            <li>2020年4月</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul class="list__style2">
+            <li>■内容</li>
+            <li>入社後釧路空港事業所に配属</li>
+            <li>航空燃料や給油施設に関する基礎知識を学び実技訓練を経て社内資格「施設従事者」を取得</li>
+            <li>航空機への燃料給油に関する基礎知識を学び、実機訓練を経て二人で作業する社内資格</li>
+            <li>「給油従事者」を取得</li>
+            <li>引き続き実機訓練を実施し、一人で航空機へ給油する社内資格「ワンマン給油従事者」を取得</li>
+            <li>千歳空港事業所第一事業所（旧給油課）へ移動の辞令、新千歳空港にて航空機への給油作業に従事</li>
+            <li>自己都合により退職</li>
+          </ul>
+        </div>
+      </div>
+  </section>
+  <section class="page">
+    <div class="main__text__content">
+      <ul class="main__list__title">
+        <li>
+          <h2><span class="main__job__title">2020年6月～2021年7月</span><span>株式会社測機社</span></h2>
+        </li>
+        <li class="main__wrapper">
+          <span class="main__job__title">主な業務内容：</span>
+          <span>ダム等の観測装置の点検及び更新工事、測量、山間部等の地すべりの観測装置の点検及び設置工事</span>
+        </li>
+      </ul>
+
+    </div>
+    <div class="main">
+      <div class="main__period">
+        <ul class="list__style1">
+          <li>■期間</li>
+          <li>2020年6月1日</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>2021年7月</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul class="list__style2">
+          <li>■内容</li>
+          <li>入社後、技術部配属</li>
+          <li>北海道内のダムや河川に設置された観測装置の取り付け工事及び保守点検
+            <span style="opacity: 0;">スペーススペーススペース</span>
+          </li>
+          <li>地すべり観測装置に取り付け工事及び保守点検</li>
+          <li>自己都合により退職</li>
+        </ul>
+      </div>
+    </div>
+
+
+    <div class="main__text__content">
+      <ul class="main__list__title">
+        <li>
+          <h2><span class="main__job__title">2021年8月～2021年11月</span><span>黒松内名水株式会社</span></h2>
+        </li>
+        <li class="main__wrapper">
+          <span>主な業務内容：</span><span>ミネラルウォーターの製造</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="main">
+      <div class="main__period">
+        <ul class="list__style1">
+          <li>■期間</li>
+          <li>2021年8月1日</li>
+          <li>&emsp;</li>
+          <li>2021年11月</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul class="list__style2">
+          <li>■内容</li>
+          <li>入社後、3カ月間の使用期間を経て正式雇用</li>
+          <li>
+            成形課に配属され、ペットボトルの製造及びペットボトルを製造する機器の点検及び整備に従事
+            <span style="opacity: 0;">スペース</span>
+          </li>
+          <li>自己都合により退職</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main__text__content">
+      <ul class="main__list__title">
+        <li>
+          <h2><span class="main__job__title">2022年2月~現在</span><span>オンラインプログラミングスクール:TECH-QUEST
+            </span></h2>
+        </li>
+        <li class="main__wrapper">
+          <span
+            class="main__job__title">学習内容：</span><span>WEBページの作成、WEBアプリの作成、Docker環境構築、Githubの使い方、VScodeの使い方を学習。</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="main">
+      <div class="main__period">
+        <ul class="list__style1">
+          <li>■期間</li>
+          <li>2022年2月から現在</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+          <li>&emsp;</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul class="list__style2">
+          <li>■内容</li>
+          <li>◇学習言語</li>
+          <li>PHP、HTML、CSS、SQL</li>
+          <li>◇制作アプリ等</li>
+          <li>
+            PHPを使用したお問い合わせフォーム、予約フォームの作成、アンケートフォームの作成
+            <span style="opacity: 0;">スペーススペ</span>
+          </li>
+          <li>HTML、CSSを使用したWEBページの作成</li>
+          <li>◇使用ツール</li>
+          <li>Dockerの環境構築及び立ち上げ</li>
+          <li>Githubの環境構築及び使用方法</li>
+          <li>VScodeの使用方法</li>
+          <li>PHP MyAdmin</li>
+        </ul>
+      </div>
+    </div>
+    </main>
+  </section>
+
+</body>
+
+</html>
